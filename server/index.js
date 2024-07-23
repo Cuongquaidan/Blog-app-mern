86c2a113f72ae9dab840b60f400e391913cb6111
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-07-23 22:42:03 +0700

    connect db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+const username = process.env.USER;
+const password = process.env.PASSWORD;
+mongoose
+    .connect(
+        `mongodb+srv://${username}:${password}@cluster0.xogyxrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    )
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 const app = express();
 
 app.listen(3000, () => {
