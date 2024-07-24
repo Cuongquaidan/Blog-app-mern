76f505bf00d656e4842c3060f65a81d2d7eefd97
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-07-24 20:10:20 +0700

    handle signup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,7 +6,7 @@ export const authController = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-        next(errorHandler(400, "All fields are required"));
+        return next(errorHandler(400, "All fields are required"));
     }
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -14,8 +14,8 @@ export const authController = async (req, res, next) => {
 
     try {
         await newUser.save();
-        res.json({ message: "Sign up successful" });
+        return res.json({ message: "Sign up successful" });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
