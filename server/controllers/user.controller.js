370c07c6b376dca12a05b056df4fcefb7347aa3a
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-07-27 12:16:52 +0700

    add delete route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -63,3 +63,17 @@ export const updateUser = async (req, res, next) => {
         next(error);
     }
 };
+export const deleteUser = async (req, res, next) => {
+    // Khác id thi khong dc xoa
+    if (req.user.id != req.params.userId) {
+        return next(
+            errorHandler(403, "You are not allowed to delete this user")
+        );
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        return res.status(200).json("User has been deleted");
+    } catch (error) {
+        next(err);
+    }
+};
