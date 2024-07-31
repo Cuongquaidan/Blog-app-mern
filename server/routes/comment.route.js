6bae54a7d1cdda7c3b1310a1d3bef0d9e538c60d
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-07-31 22:04:23 +0700

    add edit comment

--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
     createComment,
     getPostComments,
+    editComment,
     likeComment,
 } from "../controllers/comment.controller.js";
 const router = express.Router();
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 export default router;
